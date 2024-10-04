20ea57a4550fc781fd5d8a2a6bed1edfac4c7037
carlose2-glitch <carloseduardosanchezcarrasco@gmail.com>
2024-10-04 19:41:44 -0400

    arreglo del diseño del home

--- src/modules/common/components/mainPage.vue
+++ src/modules/common/components/mainPage.vue
@@ -1,5 +1,7 @@
 <template>
-  <main class="text-white">
-    <h2>Organiza tu trabajo y tu vida</h2>
+  <main class="text-white flex flex-col items-center gap-4 justify-center">
+    <h1 class="font-bold text-5xl text-center p-2">Lista de Tarea</h1>
+    <h2 class="font-bold text-5xl shadow-lg text-center p-2">Organiza tu trabajo y tu vida</h2>
+    <img src="/1582819.png" alt="img" class="h-32 w-32" />
   </main>
 </template>
